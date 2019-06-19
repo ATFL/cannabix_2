068cba48e486478273b53d1d6585bd6ece5550f1
Adithya Ravishankara <adiravishankara@gmail.com>
2019-06-19 12:52:15 -0700

    kh

--- Devices/Handheld/can_HH_test.py
+++ Devices/Handheld/can_HH_test.py
@@ -222,7 +222,7 @@ class DataPage(tk.Frame):
         stat_Valve2 = tk.Label(statusFrame, text=outValve.state, anchor='w')
         stat_Valve2.place(relx=.4,rely=0.2,relheight=0.1,relwidth=(1-0.4))
         stat_LA = tk.Label(statusFrame, text=linearActuator.state, anchor='w')
-        stat_LA.place(relx=.4,rely=0.3,relheight=0.1,relwidth=(1-0.4))
+        stat_LA.place(relx=.2,rely=0.3,relheight=0.1,relwidth=(1-0.4))
 
 
 

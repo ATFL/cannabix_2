a5f986b2d4550567e743841bd42c31af0a96b5ac
Adithya Ravishankara <adiravishankara@gmail.com>
2019-06-19 19:04:52 -0700

    fsd

--- Devices/Handheld/cannibix_components_HH.py
+++ Devices/Handheld/cannibix_components_HH.py
@@ -72,10 +72,10 @@ class MOS:
         self.GAIN = 2 / 3
         self.adc = adc
         self.channel = channel
-        self.conversion_value = (self.adc.read_adc(self.channel,gain=self.GAIN)/pow(2, 15))*6.144
+        self.conversion_value = (self.adc.read_adc(self.channel,gain=self.GAIN)
 
     def read(self):
-        self.conversion_value = (self.adc.read_adc(self.channel,gain=self.GAIN)/pow(2, 15))*6.144
+        self.conversion_value = (self.adc.read_adc(self.channel,gain=self.GAIN)
         return self.conversion_value
 
     def print(self):

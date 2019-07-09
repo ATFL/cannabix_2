6c7579aa6f0672820ff067a9a69d4b0d76be6499
adiravishankara <adiravishankara@gmail.com>
2019-07-09 10:01:06 -0700

    other test

--- Devices/LinATest.py
+++ Devices/LinATest.py
@@ -2,18 +2,18 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10,GPIO.OUT)
-GPIO.setup(8,GPIO.OUT)
+#GPIO.setup(22,GPIO.OUT)
+GPIO.setup(38,GPIO.OUT)
 
-GPIO.output(10,GPIO.HIGH)
-pwm = GPIO.PWM(8,50)
+#GPIO.output(16,GPIO.HIGH)
+pwm = GPIO.PWM(38,50)
 pwmVal = 7
 pwm.start(pwmVal)
 
 while(pwmVal != 0):
     pwmVal = float(input("What PWM: "))
     pwm.ChangeDutyCycle(pwmVal)
-    time.sleep(1)
+    time.sleep(1.5)
 
 pwm.stop()
 GPIO.cleanup()

317409fc8d1b71c9698769a1656a0279146089a7
Adithya Ravishankara <adiravishankara@gmail.com>
2019-07-11 12:32:38 -0700

    sdf

--- Devices/Handheld/can_HH_test.py
+++ Devices/Handheld/can_HH_test.py
@@ -461,7 +461,7 @@ def collect_data(xVector,yVector):
 
     np.savetxt(filename,combinedVector, fmt='%.10f', delimiter=',')
     app.frames[DataPage].filenamefiller.delete(0,END)
-
+    app.frames[DataPage].filenamefiller.insert(0,'')
     print("Data Saved")
     pass
 

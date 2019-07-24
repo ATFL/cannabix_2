6b3293b5441c313a9825e35641f66d72bbfa0527
Adithya Ravishankara <adiravishankara@gmail.com>
2019-07-24 13:04:44 -0700

    sfd

--- Devices/Handheld/can_HH_test.py
+++ Devices/Handheld/can_HH_test.py
@@ -97,6 +97,8 @@ sensing_delay_time = 5 # normall 10, time delay after beginning data acquisition
 sensing_retract_time =50# 50 # normally 60, time allowed before sensor is retracted, no longer exposed to sample
 duration_of_signal = 200#200 # normally 150, time allowed for data acquisition per test run
 
+
+test_type = 0
 ##############TESTING TIMING###################################
 # clean_chamber_purge_time = 1 # normally 30s
 # sensing_chamber_purge_time = 1 # normally 60s
@@ -235,25 +237,12 @@ class DataPage(tk.Frame):
         statusFrame = tk.LabelFrame(self, text ='Status')
         statusFrame.place(relx=0.8,rely=0.3,relheight=0.6,relwidth=0.2)
 
-        self.stat_pump_lbl = tk.Label(statusFrame, text='PUMP: ', anchor='w')
-        self.stat_pump_lbl.place(relx=0,rely=0,relheight=0.1,relwidth=(1-0.4))
-        self.stat_Valve1_lbl = tk.Label(statusFrame, text= 'Valve 1: ', anchor='w')
-        self.stat_Valve1_lbl.place(relx=0,rely=0.1,relheight=0.1,relwidth=(1-0.4))
-        self.stat_Valve2_lbl = tk.Label(statusFrame, text='Valve 2: ', anchor='w')
-        self.stat_Valve2_lbl.place(relx=0,rely=0.2,relheight=0.1,relwidth=(1-0.4))
-        self.stat_LA_lbl = tk.Label(statusFrame, text='LA: ', anchor='w')
-        self.stat_LA_lbl.place(relx=0,rely=0.3,relheight=0.1,relwidth=(1-0.4))
-
-        self.stat_pump = tk.Label(statusFrame, text=pump.state, anchor='w')
-        self.stat_pump.place(relx=.4,rely=0,relheight=0.1,relwidth=(1-0.4))
-        self.stat_Valve1 = tk.Label(statusFrame, text=inValve.state, anchor='w')
-        self.stat_Valve1.place(relx=.4,rely=0.1,relheight=0.1,relwidth=(1-0.4))
-        self.stat_Valve2 = tk.Label(statusFrame, text=outValve.state, anchor='w')
-        self.stat_Valve2.place(relx=.4,rely=0.2,relheight=0.1,relwidth=(1-0.4))
-        self.stat_LA = tk.Label(statusFrame, text=linearActuator.state, anchor='w')
-        self.stat_LA.place(relx=.2,rely=0.3,relheight=0.1,relwidth=(1-0.4))
 
 
+        self.neg_resp = RadioButton(statusFrame,text='Negative',variable = test_type,value=0)
+        self.neg_resp.place(relx = 0, rely = 0.1, relwidth = 1,relheight = .1)
+        self.pos_resp = RadioButton(statusFrame,text='Positive',variable = test_type,value=1)
+        self.pos.resp.place(relx = 0, rely = 0.2,relwidth = 1, relheight = .1)
 
         responseFrame = tk.Frame(self)
         responseFrame.place(relx=0.8,rely=0,relheight=0.3,relwidth=0.2)
@@ -268,10 +257,7 @@ class DataPage(tk.Frame):
         self.filenamefiller.place(relx=0,rely=.8,relwidth=1)
         #self.filenamefiller.set('')
 
-        self.neg_resp = RadioButton(responseFrame,text='Negative',variable = test_type,value=0)
-        self.neg_resp.place(relx = 0, rely = 0.1, relwidth = 1,relheight = .1)
-        self.pos_resp = RadioButton(responseFrame,text='Positive',variable = test_type,value=1)
-        self.pos.resp.place(relx = 0, rely = 0.2,relwidth = 1, relheight = .1)
+
 class ManualControlPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)

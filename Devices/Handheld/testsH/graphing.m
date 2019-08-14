58972e454a925238ffb5948b1e149336182c847a
Adithya Ravishankara <adiravishankara@gmail.com>
2019-08-14 15:22:43 -0700

    rtfew

---
+++ Devices/Handheld/testsH/graphing.m
@@ -0,0 +1,162 @@
+% %MEOH
+close all;
+% figure(1)
+% hold on;
+% plot(MeOH1.VarName1,MeOH1.VarName2);plot(MeOH1.VarName1,MeOH1.VarName3);plot(MeOH1.VarName1,MeOH1.VarName4);plot(MeOH1.VarName1,MeOH1.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Only Test 1");
+% hold off;
+% 
+% 
+% figure(2)
+% hold on;
+% plot(MeOH2.VarName1,MeOH2.VarName2);plot(MeOH2.VarName1,MeOH2.VarName3);plot(MeOH2.VarName1,MeOH2.VarName4);plot(MeOH2.VarName1,MeOH2.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Only Test 2");
+% hold off;
+% 
+% 
+% figure(3)
+% hold on;
+% plot(MeOH3.VarName1,MeOH3.VarName2);plot(MeOH3.VarName1,MeOH3.VarName3);plot(MeOH3.VarName1,MeOH3.VarName4);plot(MeOH3.VarName1,MeOH3.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Only Test 3");
+% hold off;
+% 
+% 
+% figure(4)
+% hold on;
+% plot(MeOH4.VarName1,MeOH4.VarName2);plot(MeOH4.VarName1,MeOH4.VarName3);plot(MeOH4.VarName1,MeOH4.VarName4);plot(MeOH4.VarName1,MeOH4.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Only Test 4");
+% hold off;
+% 
+% 
+% figure(5)
+% hold on;
+% plot(MeOH5.VarName1,MeOH5.VarName2);plot(MeOH5.VarName1,MeOH5.VarName3);plot(MeOH5.VarName1,MeOH5.VarName4);plot(MeOH5.VarName1,MeOH5.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Only Test 5");
+% hold off;
+% 
+% figure(6)
+% hold on;
+% plot(MeOH6.VarName1,MeOH6.VarName2);plot(MeOH6.VarName1,MeOH6.VarName3);plot(MeOH6.VarName1,MeOH6.VarName4);plot(MeOH6.VarName1,MeOH6.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Only Test 6");
+% hold off;
+% 
+% %MeOH + THC
+% figure(7)
+% hold on;
+% plot(BubblerTHC01.VarName1,BubblerTHC01.VarName2);plot(BubblerTHC01.VarName1,BubblerTHC01.VarName3);plot(BubblerTHC01.VarName1,BubblerTHC01.VarName4);plot(BubblerTHC01.VarName1,BubblerTHC01.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH THC Test 1");
+% hold off;
+% 
+% 
+% figure(8)
+% hold on;
+% plot(BubblerTHC02.VarName1,BubblerTHC02.VarName2);plot(BubblerTHC02.VarName1,BubblerTHC02.VarName3);plot(BubblerTHC02.VarName1,BubblerTHC02.VarName4);plot(BubblerTHC02.VarName1,BubblerTHC02.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH THC Test 2");
+% hold off;
+% 
+% 
+% figure(9)
+% hold on;
+% plot(BubblerTHC03.VarName1,BubblerTHC03.VarName2);plot(BubblerTHC03.VarName1,BubblerTHC03.VarName3);plot(BubblerTHC03.VarName1,BubblerTHC03.VarName4);plot(BubblerTHC03.VarName1,BubblerTHC03.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH THC Test 3");
+% hold off;
+% 
+% 
+% figure(10)
+% hold on;
+% plot(BubblerTHC04.VarName1,BubblerTHC04.VarName2);plot(BubblerTHC04.VarName1,BubblerTHC04.VarName3);plot(BubblerTHC04.VarName1,BubblerTHC04.VarName4);plot(BubblerTHC04.VarName1,BubblerTHC04.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH THC Test 4");
+% hold off;
+% 
+% 
+% figure(11)
+% hold on;
+% plot(BubblerTHC05.VarName1,BubblerTHC05.VarName2);plot(BubblerTHC05.VarName1,BubblerTHC05.VarName3);plot(BubblerTHC05.VarName1,BubblerTHC05.VarName4);plot(BubblerTHC05.VarName1,BubblerTHC05.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH THC Test 5");
+% hold off;
+% 
+% figure(12)
+% hold on;
+% plot(BubblerTHC06.VarName1,BubblerTHC06.VarName2);plot(BubblerTHC06.VarName1,BubblerTHC06.VarName3);plot(BubblerTHC06.VarName1,BubblerTHC06.VarName4);plot(BubblerTHC06.VarName1,BubblerTHC06.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH THC Test 6");
+% hold off;
+
+%Nictone
+% figure(13)
+% hold on;
+% plot(Nicot170ngml1.VarName1,Nicot170ngml1.VarName2);plot(Nicot170ngml1.VarName1,Nicot170ngml1.VarName3);plot(Nicot170ngml1.VarName1,Nicot170ngml1.VarName4);plot(Nicot170ngml1.VarName1,Nicot170ngml1.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Nicotine Test 1");
+% hold off;
+% 
+% 
+% figure(14)
+% hold on;
+% plot(Nicot170ngml2.VarName1,Nicot170ngml2.VarName2);plot(Nicot170ngml2.VarName1,Nicot170ngml2.VarName3);plot(Nicot170ngml2.VarName1,Nicot170ngml2.VarName4);plot(Nicot170ngml2.VarName1,Nicot170ngml2.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Nicotine Test 2");
+% hold off;
+% 
+% 
+% figure(15)
+% hold on;
+% plot(Nicot170ngml3.VarName1,Nicot170ngml3.VarName2);plot(Nicot170ngml3.VarName1,Nicot170ngml3.VarName3);plot(Nicot170ngml3.VarName1,Nicot170ngml3.VarName4);plot(Nicot170ngml3.VarName1,Nicot170ngml3.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Nicotine Test 3");
+% hold off;
+% 
+% 
+% figure(16)
+% hold on;
+% plot(Nicot170ngml4.VarName1,Nicot170ngml4.VarName2);plot(Nicot170ngml4.VarName1,Nicot170ngml4.VarName3);plot(Nicot170ngml4.VarName1,Nicot170ngml4.VarName4);plot(Nicot170ngml4.VarName1,Nicot170ngml4.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Nicotine Test 4");
+% hold off;
+% 
+% 
+% figure(17)
+% hold on;
+% plot(Nicot170ngml5.VarName1,Nicot170ngml5.VarName2);plot(Nicot170ngml5.VarName1,Nicot170ngml5.VarName3);plot(Nicot170ngml5.VarName1,Nicot170ngml5.VarName4);plot(Nicot170ngml5.VarName1,Nicot170ngml5.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Nicotine Test 5");
+% hold off;
+% 
+% figure(18)
+% hold on;
+% plot(Nicot170ngml6.VarName1,Nicot170ngml6.VarName2);plot(Nicot170ngml6.VarName1,Nicot170ngml6.VarName3);plot(Nicot170ngml6.VarName1,Nicot170ngml6.VarName4);plot(Nicot170ngml6.VarName1,Nicot170ngml6.VarName5);
+% xlabel("Time (ms)"); ylabel("Voltage (V)");
+% title("MeOH Nicotine Test 6");
+% hold off;
+
+figure(19)
+
+subplot(3,1,1);
+hold on;
+plot(MeOH5.VarName1,MeOH5.VarName2);plot(MeOH5.VarName1,MeOH5.VarName3);plot(MeOH5.VarName1,MeOH5.VarName4);plot(MeOH5.VarName1,MeOH5.VarName5);
+xlabel("Time (ms)"); ylabel("Voltage (V)");
+title("MeOH Only Test");
+hold off;
+subplot(3,1,2);
+hold on;
+plot(BubblerTHC03.VarName1,BubblerTHC03.VarName2);plot(BubblerTHC03.VarName1,BubblerTHC03.VarName3);plot(BubblerTHC03.VarName1,BubblerTHC03.VarName4);plot(BubblerTHC03.VarName1,BubblerTHC03.VarName5);
+xlabel("Time (ms)"); ylabel("Voltage (V)");
+title("MeOH THC Test 3");
+hold off;
+subplot(3,1,3);
+hold on;
+plot(Nicot170ngml4.VarName1,Nicot170ngml4.VarName2);plot(Nicot170ngml4.VarName1,Nicot170ngml4.VarName3);plot(Nicot170ngml4.VarName1,Nicot170ngml4.VarName4);plot(Nicot170ngml4.VarName1,Nicot170ngml4.VarName5);
+xlabel("Time (ms)"); ylabel("Voltage (V)");
+title("MeOH Nicotine Test 4");
+hold off;

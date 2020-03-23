8da690eb53ae23ebba26cece4b9acf21bf5eae33
Adi Ravishankara <adiravishankara@gmail.com>
2020-03-22 20:22:39 -0700

    New GUI

---
+++ Devices/can_gui_final/setup/setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# pip3 install pandas
+# pip3 install Adafruit-ADS1x15
+# pip3 install pyqt5
+# pip3 install pyqtgraph
+
+mkdir ../Data/byID/id0
+cd ../Data/byID/id0
+
+# mkdir Data
+# cd Data/
+#
+# mkdir byID
+# cd byID
+#
+# mkdir id0
+# cd id0
+
+touch id0.txt
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Cannabix
+
+This is the total document for the Cannabix Handheld Breathalyzer Devices.
+
+This folder contains all data, all code, and all information on subjects
